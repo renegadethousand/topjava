d1f61dfc0ee536aec826c8c4412d4d6092de947e
renegadethousand <renegadethousand@gmail.com>
2022-10-31 21:46:03 +0300

    HW 4

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,31 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC "),
+        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id=:userId " +
+                "AND m.dateTime >=:startDateTime AND m.dateTime <:endDateTime ORDER BY m.dateTime DESC ")
+})
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String GET_ALL = "Meal.getAllSorted";
+    public static final String GET_BETWEEN = "Meal.getBetweenHalfOpen";
+
+    @Column(name="date_time")
     private LocalDateTime dateTime;
 
+    @Column(name="description")
     private String description;
 
+    @Column(name="calories")
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
